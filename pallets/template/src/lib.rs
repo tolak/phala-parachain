40053def1613f14f136595322f7ab35adf8fb58a
Ricardo Rius <ricardo@parity.io>
2020-08-12 15:23:48 +0200

    POE pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -5,8 +5,9 @@
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// https://substrate.dev/docs/en/knowledgebase/runtime/frame
 
-use frame_support::{decl_module, decl_storage, decl_event, decl_error, dispatch, traits::Get};
+use frame_support::{decl_module, decl_storage, decl_event, decl_error, ensure, StorageMap};
 use frame_system::ensure_signed;
+use sp_std::vec::Vec;
 
 #[cfg(test)]
 mod mock;
@@ -23,13 +24,10 @@ pub trait Trait: frame_system::Trait {
 // The pallet's runtime storage items.
 // https://substrate.dev/docs/en/knowledgebase/runtime/storage
 decl_storage! {
-	// A unique name is used to ensure that the pallet's storage items are isolated.
-	// This name may be updated, but each pallet in the runtime must use a unique name.
-	// ---------------------------------vvvvvvvvvvvvvv
 	trait Store for Module<T: Trait> as TemplateModule {
-		// Learn more about declaring storage items:
-		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
+		/// The storage item for our proofs.
+		/// It maps a proof to the user who made the claim and when they made it.
+		Proofs: map hasher(blake2_128_concat) Vec<u8> => (T::AccountId, T::BlockNumber);
 	}
 }
 
@@ -37,19 +35,22 @@ decl_storage! {
 // https://substrate.dev/docs/en/knowledgebase/runtime/events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, AccountId),
+		/// Event emitted when a proof has been claimed.
+		ClaimCreated(AccountId, Vec<u8>),
+		/// Event emitted when a claim is revoked by the owner.
+		ClaimRevoked(AccountId, Vec<u8>),
 	}
 );
 
 // Errors inform users that something went wrong.
 decl_error! {
 	pub enum Error for Module<T: Trait> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		/// This proof has already been claimed
+		ProofAlreadyClaimed,
+		/// The proof does not exist, so it cannot be revoked
+		NoSuchProof,
+		/// The proof is claimed by another account, so caller can't revoke it
+		NotProofOwner,
 	}
 }
 
@@ -58,47 +59,54 @@ decl_error! {
 // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Errors must be initialized if they are used by the pallet.
+		// Initializing errors
+		// this includes information about your errors in the node's metadata.
+		// it is needed only if you are using errors in your pallet
 		type Error = Error<T>;
 
-		// Events must be initialized if they are used by the pallet.
+		// A default function for depositing events
 		fn deposit_event() = default;
 
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-			let who = ensure_signed(origin)?;
+		/// Allow a user to claim ownership of an unclaimed proof
+		#[weight = 10_000]
+		fn create_claim(origin, proof: Vec<u8>) {
+				// Verify that the incoming transaction is signed and store who the
+				// caller of this function is.
+				let sender = ensure_signed(origin)?;
 
-			// Update storage.
-			Something::put(something);
+				// Verify that the specified proof has not been claimed yet or error with the message
+				ensure!(!Proofs::<T>::contains_key(&proof), Error::<T>::ProofAlreadyClaimed);
 
-			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
-			// Return a successful DispatchResult
-			Ok(())
+				// Call the `frame_system` pallet to get the current block number
+				let current_block = <frame_system::Module<T>>::block_number();
+
+				// Store the proof with the sender and the current block number
+				Proofs::<T>::insert(&proof, (&sender, current_block));
+
+				// Emit an event that the claim was created
+				Self::deposit_event(RawEvent::ClaimCreated(sender, proof));
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		#[weight = 10_000 + T::DbWeight::get().reads_writes(1,1)]
-		pub fn cause_error(origin) -> dispatch::DispatchResult {
-			let _who = ensure_signed(origin)?;
+		/// Allow the owner to revoke their claim
+		#[weight = 10_000]
+		fn revoke_claim(origin, proof: Vec<u8>) {
+				// Determine who is calling the function
+				let sender = ensure_signed(origin)?;
 
-			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
+				// Verify that the specified proof has been claimed
+				ensure!(Proofs::<T>::contains_key(&proof), Error::<T>::NoSuchProof);
+
+				// Get owner of the claim
+				let (owner, _) = Proofs::<T>::get(&proof);
+
+				// Verify that sender of the current call is the claim owner
+				ensure!(sender == owner, Error::<T>::NotProofOwner);
+
+				// Remove claim from storage
+				Proofs::<T>::remove(&proof);
+
+				// Emit an event that the claim was erased
+				Self::deposit_event(RawEvent::ClaimRevoked(sender, proof));
 		}
 	}
 }
